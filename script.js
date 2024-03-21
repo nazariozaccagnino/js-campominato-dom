28a808cc7bff23110a442b9366fda4eea8aa4399
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-21 17:45:47 +0100

    created the function for the bombs

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 function createLittleSq (){     
     const square = document.createElement("div");
     square.className = 'littlesquare'
     return square        
+}
+function createBombs(maxnum){
+    let bombs = [];
+    while(bombs.length < 16){
+        let singleBomb = getRndInteger(1, maxnum);
+        if (!bombs.includes(singleBomb)){
+            bombs.push(singleBomb)
+        }
+        
+    } 
+    return bombs
+
 }
 
 let button = document.getElementById('startbutton')
@@ -12,10 +28,10 @@ button.addEventListener('click', function(){
     
     let mainsquare = document.getElementById('mainsquare')
     mainsquare.innerHTML = '';
-
+    
     if(choice === 1){
         for (let i = 1; i<= 100; i++){
-            let littlesq = createLittleSq();
+            let littlesq = createLittleSq();                       
             mainsquare.appendChild(littlesq);
             littlesq.addEventListener('click', function(){
                 littlesq.classList.add('squareon')
@@ -27,6 +43,12 @@ button.addEventListener('click', function(){
                 
             }, {once : true})
         }
+        let mines = createBombs(100);
+        console.log(mines)
+
+
+
+
     } if (choice === 2){
         for (let i = 1; i<= 81; i++){
             let littlesq = createLittleSq();
